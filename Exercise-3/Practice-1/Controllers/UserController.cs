9c244c19b59428c1be6c441c567a89eb381b103a
Shafin_ahmed <Ahmedshafin112233@gmail.com>
2023-11-25 03:34:27 +0600

    first Commit

---
+++ Exercise-3/Practice-1/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Practice_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practice_1.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult User()
+        {
+
+            var UserModel = new UserModel()
+            {
+                ID = 1,
+                Name = "Test",
+                Email = "Test@gmail.com",
+                Password = "password",  
+
+
+
+            };
+
+            return View(UserModel);
+
+        }
+
+
+    }
+}
